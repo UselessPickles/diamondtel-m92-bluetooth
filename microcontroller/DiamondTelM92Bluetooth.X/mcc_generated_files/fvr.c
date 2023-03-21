d20c01a31895a6e29b11a7a4fe487631705f6c25
Jeff Lau <jlau@uselesspickles.com>
2023-03-21 19:45:18 -0400

    Change FVR multiplier to account for hardware change

--- microcontroller/DiamondTelM92Bluetooth.X/mcc_generated_files/fvr.c
+++ microcontroller/DiamondTelM92Bluetooth.X/mcc_generated_files/fvr.c
@@ -57,8 +57,8 @@
 
 void FVR_Initialize(void)
 {
-    // CDAFVR 4x; FVREN enabled; TSRNG Lo_range; ADFVR off; TSEN disabled; 
-    FVRCON = 0x8C;
+    // CDAFVR 1x; FVREN enabled; TSRNG Lo_range; ADFVR off; TSEN disabled; 
+    FVRCON = 0x84;
 }
 
 bool FVR_IsOutputReady(void)
